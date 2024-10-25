4470811f1fe261b535cbf40bd72705a5af2c1266
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2024-10-25 13:32:19 +0530

    Update request_utilites.py

--- Utilities/request_utilites.py
+++ Utilities/request_utilites.py
@@ -5,31 +5,29 @@ import subprocess
 State= np.array
 def get_cypher_texts(host:str, port:int)->State:
     cyphertexts= []
-    with open('ciphertexts.txt', 'w') as file:
-
-        for x in range(256):
-            # Format the input string
-            input_str = f'{x:02x}' + '000000000000000000000000000000'
-            
-            # Run the binary and connect to its stdin/stdout
-            oracle = subprocess.Popen(
-                ['oracle/encrypt.bin'],          # Path to the binary
-                stdin=subprocess.PIPE,      # Use PIPE for input
-                stdout=subprocess.PIPE,     # Use PIPE for output
-                stderr=subprocess.PIPE      # Use PIPE for errors if needed
-            )
-            
-            # Send the input string and get output
-            stdout, stderr = oracle.communicate(input=f'{input_str}\n'.encode())
-            
-            # Decode output and parse ciphertext
-            output = stdout.decode()
-            lines = output.split('\n')
-            for line in lines:
-                if 'ciphertext' in line:
-                    ciphertext = line.split(': ')[1]
-                    cyphertexts.append(ciphertext)
-                    file.write(f'{ciphertext}\n')
+    for x in range(256):
+        # Format the input string
+        input_str = f'{x:02x}' + '000000000000000000000000000000'
+        
+        # Run the binary and connect to its stdin/stdout
+        oracle = subprocess.Popen(
+            ['encrypt.bin'],          # Path to the binary
+            stdin=subprocess.PIPE,      # Use PIPE for input
+            stdout=subprocess.PIPE,     # Use PIPE for output
+            stderr=subprocess.PIPE      # Use PIPE for errors if needed
+        )
+        
+        # Send the input string and get output
+        stdout, stderr = oracle.communicate(input=f'{input_str}\n'.encode())
+        
+        # Decode output and parse ciphertext
+        output = stdout.decode()
+        lines = output.split('\n')
+        for line in lines:
+            if 'ciphertext' in line:
+                ciphertext = line.split(': ')[1]
+                cyphertexts.append(ciphertext)
+                    
     cyphertexts= np.array(cyphertexts)
     np.save('ciphertexts.npy', cyphertexts)
     return cyphertexts
